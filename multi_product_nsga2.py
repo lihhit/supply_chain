04636684e30a90c66acfe98442cd519397fff5af
Bryce <lihhit123@gmail.com>
2020-07-23 17:08:09 +0800

    using xlsxwriter for adding data to excel automatically

--- multi_product_nsga2.py
+++ multi_product_nsga2.py
@@ -1,58 +1,138 @@
 # coding=utf-8
-
 """
+假设F公司需要购买3种产品，需求的正态分布参数如下：
+	产品A	产品B	产品C
+期望μ_(D_j )	220	240	260
+方差σ_(D_j)^2	49	36	64
+F_(D_j)^(-1) (α_(D_j )=0.05)	208.486	230.131	246.841
+F_(D_j)^(-1) (β_(D_j )=0.95)	231.514	249.869	273.159
+
+假设有5个供应商可以提供这3种产品，供货能力的正态分布N(μ_(C_ij ), σ_(C_ij)^2)参数如下：
+供应商	产品A	产品B	产品C
+1	N (55,5)	N (40,4)	N (125,25)
+2	N (80,15)	N (120,25)	N (25,1)
+3	N (65, 10)	N (60, 8)	N (150,49)
+4	N (85,6.25)	N (250, 121)	N (60,8)
+5	N (80,16)	N (100,20)	N (75,12)
+假设所有供应商i能满足j产品供应的概率为α_(C_ij )=0.95，计算从供应商i处购买j产品的数量为x_ij的上限F_(C_ij)^(-1) (1-α_(C_ij ) )=F_(C_ij)^(-1) (0.05)的值，如下图所示：
+供应商	产品A	产品B	产品C
+1	51.322	36.710	116.776
+2	73.630	111.776	23.355
+3	59.799	55.348	138.486
+4	80.888	231.907	55.348
+5	73.421	92.644	69.302
+
+供应商提供的产品单价（USD）服从正态分布：
+供应商	产品A	产品B	产品C
+1	N(200, σ_(p_11)^2)	N(180, σ_(p_12)^2)	N(240, σ_(p_13)^2)
+2	N(270, σ_(p_21)^2)	N(240, σ_(p_22)^2)	N(300, σ_(p_23)^2)
+3	N(330, σ_(p_31)^2)	N(300, σ_(p_32)^2)	N(400, σ_(p_33)^2)
+4	N(400, σ_(p_41)^2)	N(360, σ_(p_42)^2)	N(450, σ_(p_43)^2)
+5	N(350, σ_(p_51)^2)	N(350, σ_(p_52)^2)	N(420, σ_(p_53)^2)
+供应商提供产品的废品率（%）：
+供应商	产品A	产品B	产品C
+1	1	0.9	1.1
+2	0.8	0.7	1
+3	0.6	0.6	0.8
+4	0.2	0.3	0.5
+5	0.4	0.5	0.7
+
+供应商提供的产品延迟交货率(%)：
+供应商	产品A	产品B	产品C
+1	10	9	8
+2	8	7	7
+3	6	4	5
+4	3	2	1
+5	5	3	4
+将所有参数代入目标模型，
+Min Z_1= 200x_11+180x_12+240x_13+270x_21+240x_22+300x_23+330x_31+300x_32+400x_33+400x_41+360x_42+450x_43+350x_51+350x_52+420x_53
+Min Z_2=0.01x_11+0.09x_12+0.11x_13+0.008x_21+0.007x_22+0.01x_23+0.006x_31+0.006x_32+0.008x_33+0.002x_41+0.003x_42+0.005x_43+0.004x_51+0.005x_52+0.007x_53
+Min Z_3=0.1x_11+0.09x_12+0.08x_13+0.08x_21+0.07x_22+0.07x_23+0.06x_31+0.04x_32+0.05x_33+0.03x_41+0.02x_42+0.01x_43+0.05x_51+0.03x_52+0.04x_53
+约束条件，
+208.486≤x_11+x_21+x_31+x_41+x_51≤231.514
+230.131≤x_12+x_22+x_32+x_42+x_52≤249.869
+246.841≤x_13+x_23+x_33+x_43+x_53≤273.159
+x_11≤51.322，x_12≤36.710，x_13≤116.776
+x_21≤73.630，x_22≤111.776，x_23≤23.355
+x_31≤59.799，x_32≤55.348，x_33≤138.486
+x_41≤80.888，x_42≤231.907，x_43≤55.348
+x_51≤73.421，x_52≤92.644，x_53≤69.302
+
 """
 
 import random as rn
 import numpy as np
 import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif']=['SimHei']
+plt.rcParams['axes.unicode_minus']=False
+# 有中文出现的情况，需要u'内容'
 from mpl_toolkits.mplot3d import Axes3D as p3d
-
 
 
 # 产生初始种群
 def creat_initial_population(population_size):
     population = []
     while len(population) < population_size:
-        x1 = [rn.uniform(0,51.322),rn.uniform(0,36.710),rn.uniform(0,116.776)]
-        x2 = [rn.uniform(0,73.630),rn.uniform(0,111.776),rn.uniform(0,23.355)]
-        x3 = [rn.uniform(0,59.799),rn.uniform(0,55.348),rn.uniform(0,138.486)]
-        x4 = [rn.uniform(0,80.888),rn.uniform(0,231.907),rn.uniform(0,55.348)]
-        x5 = [rn.uniform(0,73.421),rn.uniform(0,92.644),rn.uniform(0,69.302)]
+        x1 = [
+            rn.randint(0, round(51.322)),
+            rn.randint(0, round(36.710)),
+            rn.randint(0, round(116.776))
+        ]
+        x2 = [
+            rn.randint(0, round(73.630)),
+            rn.randint(0, round(111.776)),
+            rn.randint(0, round(23.355))
+        ]
+        x3 = [
+            rn.randint(0, round(59.799)),
+            rn.randint(0, round(55.348)),
+            rn.randint(0, round(138.486))
+        ]
+        x4 = [
+            rn.randint(0, round(80.888)),
+            rn.randint(0, round(231.907)),
+            rn.randint(0, round(55.348))
+        ]
+        x5 = [
+            rn.randint(0, round(73.421)),
+            rn.randint(0, round(92.644)),
+            rn.randint(0, round(69.302))
+        ]
 
-
-        if 208.486 <= x1[0]+x2[0]+x3[0]+x4[0]+x5[0] <= 231.514 and 230.131 <= x1[1]+x2[1]+x3[1]+x4[1]+x5[1] <=249.869 and 246.841 <= x1[2]+x2[2]+x3[2]+x4[2]+x5[2] <= 273.159:
+        if 208.486 <= x1[0] + x2[0] + x3[0] + x4[0] + x5[
+                0] <= 231.514 and 230.131 <= x1[1] + x2[1] + x3[1] + x4[
+                    1] + x5[1] <= 249.869 and 246.841 <= x1[2] + x2[2] + x3[
+                        2] + x4[2] + x5[2] <= 273.159:
             population.append([x1, x2, x3, x4, x5])
 
     return np.array(population)
 
 
 # 计算目标函数的值
-# Min Z_1=5x_11+6x_12+4x_13 + 5x_21+4x_22+5x_23 + 4x_31+3x_32+2x_33 + 11x_41+6x_42+5x_43 + 3x_51+5x_52+9x_53
-# Min Z_2=0.05x_11+0.06x_12+0.07x_13+0.05x_21+0.03x_22+0.01x_23+0.1x_31+0.09x_32+0.09x_33 + 0.11x_41+0.07x_42+0.1x_43 + 0.1x_51+0.07x_52+0.04x_53
-# Min Z_3=10x_11+8x_12+2x_13 + 6x_21+3x_22+8x_23 + 8x_31+3x_32+9x_33 + 3x_41+4x_42+7x_43 + 8x_51+3x_52+4x_53
-
+#Min Z_1= 200x_11+180x_12+240x_13+270x_21+240x_22+300x_23+330x_31+300x_32+400x_33+400x_41+360x_42+450x_43+350x_51+350x_52+420x_53
+#Min Z_2=0.01x_11+0.09x_12+0.11x_13+0.008x_21+0.007x_22+0.01x_23+0.006x_31+0.006x_32+0.008x_33+0.002x_41+0.003x_42+0.005x_43+0.004x_51+0.005x_52+0.007x_53
+#Min Z_3=0.1x_11+0.09x_12+0.08x_13+0.08x_21+0.07x_22+0.07x_23+0.06x_31+0.04x_32+0.05x_33+0.03x_41+0.02x_42+0.01x_43+0.05x_51+0.03x_52+0.04x_53
 
 def calculate_objects_value(population):
     objects = []
     size = population.shape[0]
     for i in range(size):
         x = population[i]
-        Z1 =  5*x[0][0]+6*x[0][1]+4*x[0][2] + \
-                  5*x[1][0]+4*x[1][1]+5*x[1][2] + \
-                  4*x[2][0]+3*x[2][1]+2*x[2][2] + \
-                  11*x[3][0]+6*x[3][1]+5*x[3][2] +\
-                  3*x[4][0]+5*x[4][1]+9*x[4][2]
-        Z2 = 0.05*x[0][0]+0.06*x[0][1]+0.07*x[0][2] + \
-             0.05*x[1][0]+0.03*x[1][1]+0.01*x[1][2] + \
-             0.1*x[2][0]+0.09*x[2][1]+0.09*x[2][2] +  \
-             0.11*x[3][0]+0.07*x[3][1]+0.1*x[3][2] +  \
-             0.1*x[4][0]+0.07*x[4][1]+0.04*x[4][2]
-        Z3 = 10*x[0][0]+8*x[0][1]+2*x[0][2] + \
-             6*x[1][0]+3*x[1][1]+8*x[1][2] + \
-             8*x[2][0]+3*x[2][1]+9*x[2][2] + \
-             3*x[3][0]+4*x[3][1]+7*x[3][2] +\
-             8*x[4][0]+3*x[4][1]+4*x[4][2]
+        Z1 =  200*x[0][0]+180*x[0][1]+240*x[0][2] + \
+                  270*x[1][0]+240*x[1][1]+300*x[1][2] + \
+                  330*x[2][0]+300*x[2][1]+400*x[2][2] + \
+                  400*x[3][0]+360*x[3][1]+450*x[3][2] +\
+                  350*x[4][0]+350*x[4][1]+420*x[4][2]
+        Z2 = 0.01*x[0][0]+0.09*x[0][1]+0.11*x[0][2] + \
+             0.008*x[1][0]+0.007*x[1][1]+0.01*x[1][2] + \
+             0.006*x[2][0]+0.006*x[2][1]+0.008*x[2][2] +  \
+             0.002*x[3][0]+0.003*x[3][1]+0.005*x[3][2] +  \
+             0.004*x[4][0]+0.005*x[4][1]+0.007*x[4][2]
+        Z3 = 0.1*x[0][0]+0.09*x[0][1]+0.08*x[0][2] + \
+             0.08*x[1][0]+0.07*x[1][1]+0.07*x[1][2] + \
+             0.06*x[2][0]+0.04*x[2][1]+0.05*x[2][2] + \
+             0.03*x[3][0]+0.02*x[3][1]+0.01*x[3][2] +\
+             0.05*x[4][0]+0.03*x[4][1]+0.04*x[4][2]
         objects.append([Z1, Z2, Z3])
     return np.array(objects)
 
@@ -73,8 +153,7 @@ def calculate_crowding_distance(objects):
         # 对目标函数值从小到大排序，为了计算相邻两个个体的拥挤距离
         sorted_objects = np.sort(normed_objects[:, col])
 
-        sorted_objects_index = np.argsort(
-            normed_objects[:, col])
+        sorted_objects_index = np.argsort(normed_objects[:, col])
 
         # 对每个个体左右相减得到该个体的拥挤距离
         crowding[1:population_size - 1] = \
@@ -103,20 +182,20 @@ def reduce_by_crowding(objects, number_to_select):
         index1 = rn.randint(0, population_size - 1)
         index2 = rn.randint(0, population_size - 1)
         # 保留拥挤距离大的个体
-        if crowding_distances[index1] >= crowding_distances[
-            index2]:
-            picked_population_idx[i] = population_idx[
-                index1]
+        if crowding_distances[index1] >= crowding_distances[index2]:
+            picked_population_idx[i] = population_idx[index1]
             picked_objects[i, :] = objects[index1, :]
             population_idx = np.delete(population_idx, (index1), axis=0)
             objects = np.delete(objects, (index1), axis=0)
-            crowding_distances = np.delete(crowding_distances, (index1), axis=0)
+            crowding_distances = np.delete(crowding_distances, (index1),
+                                           axis=0)
         else:
             picked_population_idx[i] = population_idx[index2]
             picked_objects[i, :] = objects[index2, :]
             population_idx = np.delete(population_idx, (index2), axis=0)
             objects = np.delete(objects, (index2), axis=0)
-            crowding_distances = np.delete(crowding_distances, (index2), axis=0)
+            crowding_distances = np.delete(crowding_distances, (index2),
+                                           axis=0)
 
     picked_population_idx = np.asarray(picked_population_idx, dtype=int)
 
@@ -134,7 +213,8 @@ def pickup_pareto_index(objects, population_idx):
     return population_idx[pareto_front]
 
 
-def build_pareto_population(population, objects, minimum_population_size, maximum_population_size):
+def build_pareto_population(population, objects, minimum_population_size,
+                            maximum_population_size):
     unselected_population_idx = np.arange(population.shape[0])
     all_population_idx = np.arange(population.shape[0])
     pareto_front = []
@@ -158,16 +238,19 @@ def build_pareto_population(population, objects, minimum_population_size, maximu
 # 父代交叉产生子代
 def crossover(parent_1, parent_2):
     rate = rn.random()
-    child_1 = rate*parent_1+(1-rate)*parent_2
-    child_2 = rate*parent_2+(1-rate)*parent_1
-    return child_1,child_2
+    child_1 = np.rint(rate * parent_1 + (1 - rate) * parent_2)
+    child_2 = np.rint(rate * parent_2 + (1 - rate) * parent_1)
+    return child_1, child_2
+
 
 # 种群变异
 def mutate(population, mutation_probability):
     random_mutation_array = np.random.random(size=(population.shape))
     random_mutation_boolean = random_mutation_array <= mutation_probability
-    population[random_mutation_boolean] = np.logical_not(population[random_mutation_boolean])
+    population[random_mutation_boolean] = np.logical_not(
+        population[random_mutation_boolean])
     return population
+
 
 # 约束条件，
 #208.486≤x_11+x_21+x_31+x_41+x_51≤231.514
@@ -210,39 +293,108 @@ def breed_population(population):
 
 # main
 start_population_size = 500
-maximum_generation = 100
+maximum_generation = 50
 minimum_end_population_size = 450
 maximum_end_population_size = 550
 
 population = creat_initial_population(start_population_size)
 
 objects = calculate_objects_value(population)
+
 ax = plt.figure().add_subplot(111, projection='3d')
 x = objects[:, 0]
 y = objects[:, 1]
 z = objects[:, 2]
 ax.scatter(x, y, z, marker='.')
-ax.set_xlabel('Cost')
-ax.set_ylabel('Rejection')
-ax.set_zlabel('Lead time')
+ax.set_xlabel(u"成本(USD)")
+ax.set_ylabel(u'废品数(吨)')
+ax.set_zlabel(u'延迟交货数(吨)')
 
 for generation in range(maximum_generation):
     population = breed_population(population)
     objects = calculate_objects_value(population)
-    population = build_pareto_population(
-        population, objects, minimum_end_population_size, maximum_end_population_size)
+    population = build_pareto_population(population, objects,
+                                         minimum_end_population_size,
+                                         maximum_end_population_size)
 
 objects = calculate_objects_value(population)
-print(objects)
-print(population)
+
+import xlsxwriter
+size = population.shape[0]
+wb = xlsxwriter.Workbook("/Users/bryli/PycharmProjects/supply_chain/multi_data.xlsx")
+ws = wb.add_worksheet()
+header = wb.add_format({'bold': True})
+header.set_align('center')
+header.set_border(1)
+header.set_bottom(5)
+normal = wb.add_format()
+normal.set_align('center')
+normal.set_border(1)
+
+
+ws.write(0,0,u"供应商1产品A",header)
+ws.write(0,1,u"供应商1产品B",header)
+ws.write(0,2,u"供应商1产品C",header)
+
+ws.write(0,3,u"供应商2产品A",header)
+ws.write(0,4,u"供应商2产品B",header)
+ws.write(0,5,u"供应商2产品C",header)
+
+ws.write(0,6,u"供应商3产品A",header)
+ws.write(0,7,u"供应商3产品B",header)
+ws.write(0,8,u"供应商3产品C",header)
+
+ws.write(0,9,u"供应商4产品A",header)
+ws.write(0,10,u"供应商4产品B",header)
+ws.write(0,11,u"供应商4产品C",header)
+
+ws.write(0,12,u"供应商5产品A",header)
+ws.write(0,13,u"供应商5产品B",header)
+ws.write(0,14,u"供应商5产品C",header)
+
+ws.write(0,15,u"成本(USD)",header)
+ws.write(0,16,u"废品数(吨)",header)
+ws.write(0,17,u"延迟交货数(吨)",header)
+
+for idx in range(0,size):
+    raw = idx+1
+    ws.write(raw, 0, population[idx][0][0],normal)
+    ws.write(raw, 1, population[idx][0][1], normal)
+    ws.write(raw, 2, population[idx][0][2], normal)
+
+    ws.write(raw, 3, population[idx][1][0],normal)
+    ws.write(raw, 4, population[idx][1][1], normal)
+    ws.write(raw, 5, population[idx][1][2], normal)
+
+    ws.write(raw, 6, population[idx][2][0],normal)
+    ws.write(raw, 7, population[idx][2][1], normal)
+    ws.write(raw, 8, population[idx][2][2], normal)
+
+    ws.write(raw, 9, population[idx][3][0],normal)
+    ws.write(raw, 10, population[idx][3][1], normal)
+    ws.write(raw, 11, population[idx][3][2], normal)
+
+    ws.write(raw, 12, population[idx][4][0],normal)
+    ws.write(raw, 13, population[idx][4][1], normal)
+    ws.write(raw, 14, population[idx][4][2], normal)
+
+    ws.write(raw, 15, objects[idx][0],normal)
+    ws.write(raw, 16, objects[idx][1],normal)
+    ws.write(raw, 17, objects[idx][2],normal)
+
+widths = [16, 16,16, 16,16, 16,16, 16,16, 16,16, 16,16, 16,16, 16,16, 16]
+for i in range(len(widths)):
+    ws.set_column('%c:%c' % (chr(65 + i), chr(65 + i)), widths[i])
+
+wb.close()
 ax = plt.figure().add_subplot(111, projection='3d')
 x = objects[:, 0]
 y = objects[:, 1]
 z = objects[:, 2]
 ax.scatter(x, y, z, marker='.')
 
-ax.set_xlabel('Cost')
-ax.set_ylabel('Rejection')
-ax.set_zlabel('Lead time')
+ax.set_xlabel(u"成本(USD)")
+ax.set_ylabel(u'废品数(吨)')
+ax.set_zlabel(u'延迟交货数(吨)')
 
 plt.show()
--- single_product_nsga2.py
+++ single_product_nsga2.py
@@ -1,36 +1,35 @@
-# coding=utf-8
-
+# coding:utf-8
 """
-假定F公司需要采购某一种产品，总的需求量D是250, 目前有五个供应商可供选择，忽略这五个供应商的优劣等级，五个供应商的供货能力如下表所示：
+假定F公司采用定期订货法采购某一种生产原料，月需求量D是150吨（含±10吨的允差），目前有五个供应商可供选择，忽略这五个供应商服务水平的优劣等级，五个供应商的供货能力如下表所示：
 表4.1 单产品的各供应商的供货能力
-供应商i	产品单价p_i(RMB)	废品率q_i(%)	最大供货量d_i(unit)	交货时间
-（hours/unit）
-1	5	5	50	10
-2	5	5	90	5
-3	4	10	70	8
-4	10	10	80	3
-5	10	10	70	8
+供应商i	产品FOB单价p_i(USD/吨)	废品率q_i(%)	最小起订量M_i(吨)	最大供货量S_i(吨)	延迟交货率
+（%）
+1	200	1	10	80	9
+2	270	0.8	10	50	7
+3	330	0.6	5	20	4
+4	400	0.2	10	50	2
+5	350	0.4	5	30	3
 
 将参数代入目标模型，
-MinZ_1=5x_1+5x_2+4x_3+10x_4+10x_5
-Min Z_2= 0.05x_1+0.05x_2+0.1x_3+0.1x_4+0.1x_5
-Min Z_3= 10x_1+5x_2+8x_3+3x_4+8x_5
+MinZ_1=200x_1 y_1+270x_2 y_2+330x_3 y_3+400x_4 y_4+350x_5 y_5
+Min Z_2= 0.01x_1 y_1+0.008x_2 y_2+0.006x_3 y_3+0.002x_4 y_4+0.004x_5 y_5
+Min Z_3= 0.09x_1 y_1+0.07x_2 y_2+0.04x_3 y_3+0.02x_4 y_4+0.03x_5 y_5
 约束条件:
-x_1+x_2+x_3+x_4+x_5=250
-x_1≤50,x_2≤90,x_3≤70,x_4≤80,x_5≤70
-将约束条件x_5=250-(x_1+x_2+x_3+x_4 )代入三个目标函数，得到
-MinZ_1=2500-5x_1-5x_2-6x_3
-Min Z_2= 25-0.05x_1-0.05x_2
-Min Z_3= 2000+2x_1-3x_2-5x_4
-约束条件也变更为：
-180≤x_1+x_2+x_3+x_4≤250
-x_1≤50,x_2≤90,x_3≤70,x_4≤80
+140≤x_1 y_1+x_2 y_2+x_3 y_3+x_4 y_4+x_5 y_5≤160
+10≤x_1≤80,10≤x_2≤50,5≤x_3≤20,10≤x_4≤50,5≤x_5≤30
+y_1,y_2,y_3,y_4,y_5∈[0,1]
+
 """
 
 import random as rn
 import numpy as np
 import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif']=['SimHei']
+plt.rcParams['axes.unicode_minus']=False
+# 有中文出现的情况，需要u'内容'
 from mpl_toolkits.mplot3d import Axes3D as p3d
+
+
 
 
 def individual_from_values_to_gene(values):
@@ -46,6 +45,7 @@ def individual_from_gene_to_value(gene):
     values.append(int("".join(str(i) for i in gene[8:16]), 2))
     values.append(int("".join(str(i) for i in gene[16:24]), 2))
     values.append(int("".join(str(i) for i in gene[24:32]), 2))
+    values.append(int("".join(str(i) for i in gene[32:40]), 2))
     return values
 
 
@@ -53,31 +53,43 @@ def individual_from_gene_to_value(gene):
 def creat_initial_population(population_size):
     population = []
     while len(population) < population_size:
-        x1 = np.random.random_integers(0, 50)
-        x2 = np.random.random_integers(0, 90)
-        x3 = np.random.random_integers(0, 70)
-        x4 = np.random.random_integers(0, 80)
+        x1 = rn.randint(10,80)
+        x2 = rn.randint(10,50)
+        x3 = rn.randint(5,20)
+        x4 = rn.randint(10,50)
+        x5 = rn.randint(5,30)
 
-        temp = x1 + x2 + x3 + x4
-        if temp >= 180 and temp <= 250:
-            population.append(([x1, x2, x3, x4]))
+        y1 = rn.randint(0, 1)
+        y2 = rn.randint(0, 1)
+        y3 = rn.randint(0, 1)
+        y4 = rn.randint(0, 1)
+        y5 = rn.randint(0, 1)
+
+        temp = x1*y1+x2*y2+x3*y3+x4*y4+x5*y5
+        if 140 <= temp <= 150:
+            population.append(([x1*y1, x2*y2, x3*y3, x4*y4, x5*y5]))
 
     return np.array(population)
 
 
 # 计算目标函数的值
-# Z_1=15y_1+10y_2+6y_3+5y_4+4y_5+ 2500-5x_1-5x_2-6x_3
-# Z_2= 25-0.05x_1-0.05x_2
-# Z_3= 2000+2x_1-3x_2-5x_4
+# MinZ_1=420x_1 y_1+427x_2 y_2+433x_3 y_3+440x_4 y_4+435x_5 y_5
+# Min Z_2= 0.0005x_1 y_1+0.0004x_2 y_2+0.0003x_3 y_3+0.0002x_4 y_4+0.0003x_5 y_5
+# Min Z_3= 15y_1+12y_2+10y_3+14y_4+10y_5
+
 
 def calculate_objects_value(population):
     objects = []
     size = population.shape[0]
     for i in range(size):
         x = population[i]
-        Z1 = 2500 - 5 * x[0] - 5 * x[1] - 6 * x[2]
-        Z2 = 25 - 0.05 * x[0] - 0.05 * x[1]
-        Z3 = 2000 + 2 * x[0] - 3 * x[1] - 5 * x[3]
+        y = []
+        for j in range(0,5):
+            if x[j]>0: y.append(1)
+            else: y.append(0)
+        Z1 = 200*x[0]+270*x[1]+330*x[2]+400*x[3]+350*x[4]
+        Z2 = 0.01*x[0]+0.008*x[1]+0.006*x[2]+0.002*x[3]+0.004*x[4]
+        Z3 = 0.09*x[0]+0.07*x[1]+0.04*x[2]+0.02*x[3]+0.03*x[4]
         objects.append([Z1, Z2, Z3])
     return np.array(objects)
 
@@ -98,8 +110,7 @@ def calculate_crowding_distance(objects):
         # 对目标函数值从小到大排序，为了计算相邻两个个体的拥挤距离
         sorted_objects = np.sort(normed_objects[:, col])
 
-        sorted_objects_index = np.argsort(
-            normed_objects[:, col])
+        sorted_objects_index = np.argsort(normed_objects[:, col])
 
         # 对每个个体左右相减得到该个体的拥挤距离
         crowding[1:population_size - 1] = \
@@ -128,20 +139,20 @@ def reduce_by_crowding(objects, number_to_select):
         index1 = rn.randint(0, population_size - 1)
         index2 = rn.randint(0, population_size - 1)
         # 保留拥挤距离大的个体
-        if crowding_distances[index1] >= crowding_distances[
-            index2]:
-            picked_population_idx[i] = population_idx[
-                index1]
+        if crowding_distances[index1] >= crowding_distances[index2]:
+            picked_population_idx[i] = population_idx[index1]
             picked_objects[i, :] = objects[index1, :]
             population_idx = np.delete(population_idx, (index1), axis=0)
             objects = np.delete(objects, (index1), axis=0)
-            crowding_distances = np.delete(crowding_distances, (index1), axis=0)
+            crowding_distances = np.delete(crowding_distances, (index1),
+                                           axis=0)
         else:
             picked_population_idx[i] = population_idx[index2]
             picked_objects[i, :] = objects[index2, :]
             population_idx = np.delete(population_idx, (index2), axis=0)
             objects = np.delete(objects, (index2), axis=0)
-            crowding_distances = np.delete(crowding_distances, (index2), axis=0)
+            crowding_distances = np.delete(crowding_distances, (index2),
+                                           axis=0)
 
     picked_population_idx = np.asarray(picked_population_idx, dtype=int)
 
@@ -159,7 +170,8 @@ def pickup_pareto_index(objects, population_idx):
     return population_idx[pareto_front]
 
 
-def build_pareto_population(population, objects, minimum_population_size, maximum_population_size):
+def build_pareto_population(population, objects, minimum_population_size,
+                            maximum_population_size):
     unselected_population_idx = np.arange(population.shape[0])
     all_population_idx = np.arange(population.shape[0])
     pareto_front = []
@@ -186,24 +198,29 @@ def crossover(parent_1, parent_2):
     gene_parent_2 = individual_from_values_to_gene(parent_2)
     chromosome_length = len(gene_parent_1)
     crossover_point = rn.randint(1, chromosome_length - 1)
-    gene_child_1 = np.hstack((gene_parent_1[0:crossover_point],
-                              gene_parent_2[crossover_point:]))
-    gene_child_2 = np.hstack((gene_parent_2[0:crossover_point],
-                              gene_parent_1[crossover_point:]))
-    return individual_from_gene_to_value(gene_child_1), individual_from_gene_to_value(gene_child_2)
+    gene_child_1 = np.hstack(
+        (gene_parent_1[0:crossover_point], gene_parent_2[crossover_point:]))
+    gene_child_2 = np.hstack(
+        (gene_parent_2[0:crossover_point], gene_parent_1[crossover_point:]))
+    return individual_from_gene_to_value(
+        gene_child_1), individual_from_gene_to_value(gene_child_2)
 
 
 # 种群变异
 def mutate(population, mutation_probability):
     random_mutation_array = np.random.random(size=(population.shape))
     random_mutation_boolean = random_mutation_array <= mutation_probability
-    population[random_mutation_boolean] = np.logical_not(population[random_mutation_boolean])
+    population[random_mutation_boolean] = np.logical_not(
+        population[random_mutation_boolean])
     return population
 
+# 5000≤x_1≤8000,3000≤x_2≤5000,1000≤x_3≤2000,3000≤x_4≤5000,1500≤x_5≤3000
+# 14925≤x_1 y_1+x_2 y_2+x_3 y_3+x_4 y_4+x_5 y_5≤15075
 
 def is_right_individual(values):
     temp = sum(values)
-    if values[0] <= 50 and values[1] <= 90 and values[2] <= 70 and values[3] <= 80 and temp > 180 and temp < 250:
+    if 10<=values[0] <= 80 and 10<=values[1] <= 50 and 5<=values[2] <= 20 \
+            and 10<=values[3] <= 50 and 5<=values[4] <= 30 and 140 <= temp <= 160:
         return True
     return False
 
@@ -220,7 +237,8 @@ def breed_population(population):
             new_population.append(child_1)
         if is_right_individual(child_2):
             new_population.append(child_2)
-    population = np.vstack((population, np.array(new_population)))
+    if new_population:
+        population = np.vstack((population, np.array(new_population)))
     population = np.unique(population, axis=0)
     return population
 
@@ -234,31 +252,73 @@ maximum_end_population_size = 550
 population = creat_initial_population(start_population_size)
 
 objects = calculate_objects_value(population)
+
 ax = plt.figure().add_subplot(111, projection='3d')
 x = objects[:, 0]
 y = objects[:, 1]
 z = objects[:, 2]
 ax.scatter(x, y, z, marker='.')
-ax.set_xlabel('Cost')
-ax.set_ylabel('Rejection')
-ax.set_zlabel('Lead time')
+ax.set_xlabel(u"成本(USD)")
+ax.set_ylabel(u'废品数(吨)')
+ax.set_zlabel(u'延迟交货数(吨)')
 
 for generation in range(maximum_generation):
     population = breed_population(population)
     objects = calculate_objects_value(population)
-    population = build_pareto_population(
-        population, objects, minimum_end_population_size, maximum_end_population_size)
+    population = build_pareto_population(population, objects,
+                                         minimum_end_population_size,
+                                         maximum_end_population_size)
 
 objects = calculate_objects_value(population)
+
+import xlsxwriter
+size = population.shape[0]
+wb = xlsxwriter.Workbook("/Users/bryli/PycharmProjects/supply_chain/single_data.xlsx")
+ws = wb.add_worksheet()
+header = wb.add_format({'bold': True})
+header.set_align('center')
+header.set_border(1)
+header.set_bottom(5)
+normal = wb.add_format()
+normal.set_align('center')
+normal.set_border(1)
+
+
+ws.write(0,0,u"供应商1",header)
+ws.write(0,1,u"供应商2",header)
+ws.write(0,2,u"供应商3",header)
+ws.write(0,3,u"供应商4",header)
+ws.write(0,4,u"供应商5",header)
+ws.write(0,5,u"成本(USD)",header)
+ws.write(0,6,u"废品数(吨)",header)
+ws.write(0,7,u"延迟交货数(吨)",header)
+
+for idx in range(0,size):
+    raw = idx+1
+    ws.write(raw, 0, population[idx][0],normal)
+    ws.write(raw, 1, population[idx][1],normal)
+    ws.write(raw, 2, population[idx][2],normal)
+    ws.write(raw, 3, population[idx][3],normal)
+    ws.write(raw, 4, population[idx][4],normal)
+    ws.write(raw, 5, objects[idx][0],normal)
+    ws.write(raw, 6, objects[idx][1],normal)
+    ws.write(raw, 7, objects[idx][2],normal)
+
+widths = [8, 8, 8, 8, 8, 16, 16,16]
+for i in range(len(widths)):
+    ws.set_column('%c:%c' % (chr(65 + i), chr(65 + i)), widths[i])
+
+wb.close()
 ax = plt.figure().add_subplot(111, projection='3d')
 x = objects[:, 0]
 y = objects[:, 1]
 z = objects[:, 2]
 ax.scatter(x, y, z, marker='.')
 
-ax.set_xlabel('Cost')
-ax.set_ylabel('Rejection')
-ax.set_zlabel('Lead time')
+ax.set_xlabel(u"成本(USD)")
+ax.set_ylabel(u'废品数(吨)')
+ax.set_zlabel(u'延迟交货数(吨)')
 
 plt.show()
+
 print("end")
